a3b9dbe9b0123fb69ca44c0834ffeb55a4e20cb2
themaks16 <80203502+themaks16@users.noreply.github.com>
2026-09-08 18:11:36 +0300

    Fix profile styles import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './Profile.css'
 import { supabase } from './lib/supabase'
 import App from './App.jsx'
 
